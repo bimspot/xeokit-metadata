40f7890bbbb7c943edb5f18a39be53da6876aefd
Adam Eri <adam.eri@blackmirror.media>
2019-07-27 19:32:51 +0200

    Added distribution script.

---
+++ dist/README.md
@@ -0,0 +1,14 @@
+# Distributions of `xeokit-metadata`
+
+Archives (`zip` and `tar.gz`) are created with the bash script `publish.sh`
+for the following runtimes (.NET Core 2.0 or later versions):
+
+* `linux-x64` (Most desktop distributions like CentOS, Debian, Fedora,
+Ubuntu and derivatives)
+* `linux-musl-x64` (Lightweight distributions using musl like Alpine Linux)
+* `linux-arm` (Linux distributions running on ARM like Raspberry Pi)
+* `win-x64`
+* `win-x86`
+* `win-arm`
+* `win-arm64`
+* `osx-x64` (Minimum OS version is macOS 10.12 Sierra)
---
+++ dist/publish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RuntimeIdentifiers=(
+  linux-x64
+  linux-musl-x64
+  linux-arm
+  win-x64
+  win-x86
+  win-arm
+  win-arm64
+  osx-x64)
+
+printf "Publishing Framework-dependent executable.\n\n"
+
+for id in "${RuntimeIdentifiers[@]}"
+do
+  printf "[x] Packaging for runtime: $id\n\n"
+
+  dotnet publish ../xeokit/xeokit-metadata/xeokit-metadata.csproj \
+    -c Release \
+    -r $id \
+    --self-contained false \
+    --output ../../dist/$id
+
+  zip -r $id.zip $id
+  tar -zcvf $id.tar.gz $id
+  rm -rf $id
+
+  printf "\n\n"
+done
+
+printf "Publishing completed.\n\n"
