e90567d142879bd22432775a983c077e7ef46a81
Adam Eri <adam.eri@blackmirror.media>
2019-07-27 14:37:22 +0200

    Updated to `latest` language version. Refactored all conversion methods to the model. Added JSON schema for the generated JSON. Added documentation.

--- xeokit/Hierarchy/Program.cs
+++ xeokit/Hierarchy/Program.cs
@@ -1,95 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Xbim.Ifc;
-using Xbim.Ifc2x3.Interfaces;
 
 namespace Hierarchy {
-  static class Program {
+  /// <summary>
+  /// This command line application extracts the hierarchical structure of
+  /// the building elements and creates a JSON output compatible with the
+  /// xeokit-sdk's metadata model.
+  /// See: https://github.com/xeokit/xeokit-sdk/wiki/Viewing-BIM-Models-Offline
+  ///
+  /// The program takes two arguments, first is the path to the IFC file, the
+  /// second one is the path to the output JSON. Currently only IFC 2x3 is
+  /// supported.
+  ///
+  /// Xbim's SDK is used to process the IFC file.
+  /// 
+  /// The JSON schema can be found in the sources:
+  /// https://github.com/bimspot/xeokit-metadata-utils
+  /// </summary>
+  internal static class Program {
     private static void Main(string[] args) {
-      
       if (args.Length < 2) {
-        System
-          .Console
+        Console
           .WriteLine("Please specify the path to the IFC and the output json.");
+        Console.WriteLine(@"
+          Usage:
+          
+          $ IfcHierarchy /path/to/some.ifc /path/to/output.json
+
+        ");
         Environment.Exit(1);
       }
-      
+
       var ifcPath = args[0];
+      if (File.Exists(ifcPath) == false) {
+        Console
+          .WriteLine("The IFC file does not exists at path: {0}", ifcPath);
+        Environment.Exit(1);
+      }
+
+      // TODO: create path?
       var jsonPath = args[1];
-      
-      using (var model = IfcStore.Open(ifcPath)) {
-        var project = model.Instances.FirstOrDefault<IIfcProject>();
-        
-        var metaModel = new MetaModel();
-        metaModel.Id = project.Name;
-        metaModel.ProjectId = project.GlobalId;
-          
-        var metaObjects = CalculateHierarchy(project);
-        metaModel.MetaObjects = metaObjects;
 
-        WriteToJson(metaModel, jsonPath);
+      try {
+        var metaModel = MetaModel.FromIfc(ifcPath);
+        metaModel.ToJson(jsonPath);
+        Environment.Exit(0);
       }
-    }
-
-    static List<MetaObject> CalculateHierarchy(IIfcObjectDefinition 
-    objectDefinition) {
-
-      var metaObjects = new List<MetaObject>();
-
-      var parentObject = new MetaObject {
-        Id = objectDefinition.GlobalId,
-        Name = objectDefinition.Name,
-        Type = objectDefinition.GetType().Name
-      };
-
-      metaObjects.Add(parentObject);
-
-      var spatialElement = objectDefinition as IIfcSpatialStructureElement;
-
-      if (spatialElement != null) {
-        var containedElements = spatialElement
-          .ContainsElements
-          .SelectMany(rel => rel.RelatedElements);
-        
-        foreach (var element in containedElements) {
-          var mo = new MetaObject {
-            Id = element.GlobalId,
-            Name = element.Name,
-            Type = element.GetType().Name,
-            Parent = spatialElement.GlobalId
-          };
-          metaObjects.Add(mo);
-        }
-      }
-
-      var relatedObjects = objectDefinition
-        .IsDecomposedBy
-        .SelectMany(r => r.RelatedObjects);
-
-      foreach (var item in relatedObjects) {
-        var children = CalculateHierarchy(item);
-        metaObjects.AddRange(children);
-      }
-
-      return metaObjects;
-    }
-
-    static async void WriteToJson(MetaModel metaModel, string jsonPath) {
-      var contractResolver = new DefaultContractResolver {
-        NamingStrategy = new CamelCaseNamingStrategy()
-      };
-      var settings = new JsonSerializerSettings {
-        ContractResolver = contractResolver,
-        Formatting = Formatting.Indented
-      };
-      
-      using (StreamWriter outputFile = new StreamWriter(jsonPath)) {
-        var output = JsonConvert.SerializeObject(metaModel, settings);
-        await outputFile.WriteAsync(output);
+      catch (Exception e) {
+        Console.WriteLine(e);
+        Environment.Exit(1);
       }
     }
   }
