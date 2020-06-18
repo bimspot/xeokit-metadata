488c3de4a123b9f8557ad0822cb3cf549c071214
Balint Bende <balint@bimspot.io>
2020-06-18 16:12:57 +0200

    BIMSPOT-1300: upgrade .net core from 2.2 to 3.1, upgrade nuget packages

--- xeokit/xeokit-metadata/MetaModel.cs
+++ xeokit/xeokit-metadata/MetaModel.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Xbim.Common.Step21;
 using Xbim.Ifc;
 using Xbim.Ifc4.Interfaces;
 
